174653146711e6e39811570666ec3f299d89a4bc
thanh-hang08 <156096537+thanh-hang08@users.noreply.github.com>
2024-01-21 22:51:53 +0700

    Update Practice 4.sql

--- Practice 4.sql
+++ Practice 4.sql
@@ -8,3 +8,10 @@ SUM(CASE
   END) AS mobile_views
  FROM viewership;
 --- Ex2
+SELECT x,y,z,
+CASE
+    WHEN x+y>z and x+z>y and y+z>x THEN 'Yes' 
+    ELSE 'No' 
+    END AS triangle
+FROM Triangle
+---- Ex 3
