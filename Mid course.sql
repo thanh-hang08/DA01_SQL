2c5ce0e17614e513349cad752414ad05645cf6c8
thanh-hang08 <156096537+thanh-hang08@users.noreply.github.com>
2024-01-29 00:42:37 +0700

    Update Mid course.sql

--- Mid course.sql
+++ Mid course.sql
@@ -14,3 +14,4 @@ INNER JOIN film_category ON film.film_id=film_category.film_id
 INNER JOIN category ON film_category.category_id=category.category_id
 WHERE category.name ='Drama' or category.name ='Sports'
 ORDER BY film.length desc
+--- Ex 4
