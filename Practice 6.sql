1a4aeae17b86af0c6655f9052831ca2f2e08bb5c
thanh-hang08 <156096537+thanh-hang08@users.noreply.github.com>
2024-02-01 22:39:28 +0700

    Update Practice 6.sql

--- Practice 6.sql
+++ Practice 6.sql
@@ -8,14 +8,31 @@ SELECT
 COUNT (DISTINCT company_id) 
 FROM job_hiring 
 WHERE job_count>1
---- Ex 2
-SELECT 
-  category, 
-  product,
-  (SELECT Max(spend) AS total
-  FROM product_spend table1
-  WHERE table1.category=table2.category
-  GROUP BY category)
-FROM product_spend table2
+--- Ex 2 with CTEs
+WITH xx AS 
+(SELECT category, product, sum(spend) AS total_spend
+FROM product_spend
+WHERE EXTRACT(year FROM transaction_date)=2022
+GROUP BY category, product)
+(SELECT * FROM xx
+WHERE category='appliance'
+ORDER BY total_spend DESC
+limit 2)
+ UNION ALL
+(SELECT * FROM xx
+WHERE category='electronics'
+ORDER BY total_spend DESC
+limit 2)
+--- Ex 2 with ranking
+WITH ranking_table AS
+(SELECT category, product,
+sum(spend) AS total_spend,
+RANK() OVER(PARTITION BY category ORDER BY sum(spend) DESC) AS ranking
+FROM product_spend
 WHERE EXTRACT(YEAR FROM transaction_date) = 2022
-GROUP BY category, product
+GROUP BY category, product)
+SELECT category, product, total_spend
+FROM ranking_table
+WHERE ranking<=2
+GROUP BY category, product, total_spend, ranking
+ORDER BY category, ranking 
